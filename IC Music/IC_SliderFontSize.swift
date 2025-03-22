4878b3edc493c3c415d5f02f126d5755cd163624
Andreas Franke <a.franke@email.de>
2025-03-22 17:41:28 +0100

    added label to slider

--- IC Music/IC_SliderFontSize.swift
+++ IC Music/IC_SliderFontSize.swift
@@ -11,12 +11,16 @@ struct IC_SliderFontSize: View {
   @Binding var fontoSize: Double
 
   var body: some View {
+    HStack(spacing: 5) {
+      Text("Font")
+        .font(.system(size: fontoSize * 0.5))
     Slider(value: $fontoSize, in: 0 ... 60, step: 1).padding()
-      .onAppear {//for a smaler slider
-        let progressCircleConfig = UIImage.SymbolConfiguration(scale: .small)
-        UISlider.appearance()
-          .setThumbImage(UIImage(systemName: "circle.fill",
-                                 withConfiguration: progressCircleConfig), for: .normal)
+        .onAppear {//for a smaler slider
+          let progressCircleConfig = UIImage.SymbolConfiguration(scale: .small)
+          UISlider.appearance()
+            .setThumbImage(UIImage(systemName: "circle.fill",
+                                   withConfiguration: progressCircleConfig), for: .normal)
+        }
       }
   }
 }
